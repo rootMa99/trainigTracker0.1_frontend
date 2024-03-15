a390a2cd100441024f0ae86a00ba0c6b475d9b7f
anass zeroual <anasszeroual09@gmail.com>
2024-03-15 12:55:38 +0000

    963

---
+++ src/components/admin/Employee.js
@@ -0,0 +1,18 @@
+import c from "./Employee.module.css";
+import imglogo from "../../assets/aptiv-logo.svg";
+
+const Employee = (p) => {
+  return (
+    <div className={c["form-container"]}>
+      <div className={c.logo}>
+        <img src={imglogo} alt="logo for aptiv" />
+      </div>
+      <div className={c.employeeT}>
+      <span></span>
+      <h1>Employee Info</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Employee;
--- src/components/admin/Home.js
+++ src/components/admin/Home.js
@@ -1,20 +1,22 @@
 import React, { useState } from "react";
 import aptivBgVid from "../../assets/videointro.mp4";
 import c from "./Home.module.css";
-import { useNavigate } from "react-router-dom";
+import Employee from "./Employee";
+import BackDrop from "../UI/BackDrop";
+
 
 const Home = (p) => {
   const [typing, setTyping] = useState(false);
   const [value, setValue] = useState("");
-  const navigate = useNavigate();
+  const [show, setShow]=useState(false);
 
   const onChangeHandler = (e) => {
     const value = e.target.value;
     if (/^\d*$/.test(value)) {
-      if (value.trim() !== "") {
+      if (value.trim() !== "" && +value !== 0) {
         setTyping(true);
         setValue(value);
-      } else if (value.trim() === "" || +value <= 0) {
+      } else if (value.trim() === "" || +value === 0) {
         setTyping(false);
         setValue(e.target.value);
       }
@@ -27,22 +29,31 @@ const Home = (p) => {
   };
   const onClickHandler = (e) => {
     setTyping(false);
-    setValue("");
-    navigate(`/home/${value}`);
+    setShow(true);
+    //setValue("");
   };
-
+  const onClose=e=>{
+    setShow(false);
+    setValue("");
+  }
   const classBtn = !typing ? c.buttonOut : c.buttonIn;
   return (
     <React.Fragment>
       <video className={c.videoBg} autoPlay loop playsInline muted>
         <source src={aptivBgVid} type="video/mp4" />
       </video>
+      {
+        show && <BackDrop click={onClose}/>
+      }
+      {
+        show && <Employee />
+      }
       <div className={c.inputHolder}>
         <h1>
           <span>The Bridge to Success:</span> Training and Transformation
         </h1>
         <input
-          type="number"
+          type="text"
           placeholder="Search By matricule"
           pattern="[0-9]*"
           value={value}
