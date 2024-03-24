5377d3f5b3dce3d7e857c2691353610eee90927a
anass zeroual <anasszeroual09@gmail.com>
2024-03-24 13:20:04 +0000

    templo

--- src/components/admin/Training.js
+++ src/components/admin/Training.js
@@ -21,37 +21,37 @@ const Training = ({ data }) => {
       {data !== undefined && (
         <div className={c.trainingH} onClick={clickHandler}>
           <div className={c.trainingD}>
-            <div className={c.dataT}>
+            <div className={c.dataT} style={{width:"40%"}}>
               <span>training title</span>
               <h3>{data.trainingTitle}</h3>
             </div>
-            <div className={c.dataT}>
+            <div className={c.dataT} style={{width:"30%"}}>
               <span>training type</span>
               <h3>{data.trainingType}</h3>
             </div>
-            <div className={c.dataT}>
+            <div className={c.dataT} style={{width:"10%"}}>
               <span>modality</span>
               <h3>{data.modalite}</h3>
             </div>
-            <div className={c.dataT}>
+            <div className={c.dataT} style={{width:"10%"}}>
               <span>ts/h</span>
               <h3>{data.dph}</h3>
             </div>
           </div>
           <div className={c.trainingDi}>
-            <div className={c.dataT}>
+            <div className={c.dataT} style={{width:"25%"}}>
               <span>sd</span>
               <h3>{data.ddb}</h3>
             </div>
-            <div className={c.dataT}>
+            <div className={c.dataT} style={{width:"25%"}}>
               <span>ed</span>
               <h3>{data.ddf}</h3>
             </div>
-            <div className={c.dataT}>
+            <div className={c.dataT} style={{width:"25%"}}>
               <span>provider</span>
               <h3>{data.prestataire}</h3>
             </div>
-            <div className={c.dataT}>
+            <div className={c.dataT} style={{width:"25%"}}>
               <span>trainer</span>
               <h3>{data.formatteur}</h3>
             </div>
