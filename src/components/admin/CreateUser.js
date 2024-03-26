295fd40f7e5e4bebf1ef794921aef7009432c37c
anass zeroual <anasszeroual09@gmail.com>
2024-03-26 12:00:08 +0000

    possible

--- src/components/admin/CreateUser.js
+++ src/components/admin/CreateUser.js
@@ -247,6 +247,19 @@ const CreateUser = (p) => {
         </div>
         <div className={c.blob}></div>
       </div>) }
+       {users.length>0 && users.map(m=><div className={c.card}>
+        <div className={c.bg}>
+          <div className={c.detailsC}>
+            <h4>user name</h4>
+            <h3>{m.userName}</h3>
+          </div>
+          <div className={c.detailsC}>
+            <h4>role</h4>
+            <h3>{m.role}</h3>
+          </div>
+        </div>
+        <div className={c.blob}></div>
+      </div>) }
       </div>
     </React.Fragment>
   );
